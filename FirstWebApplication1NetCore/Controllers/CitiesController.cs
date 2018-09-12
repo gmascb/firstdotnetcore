00152d10c21542f9bab640501391c5f3d64133a7
Guilherme Mascarenhas <gmascb@gmail.com>
2018-09-12 19:55:33 -0300

    Adicionando Controller Model e Exemplos Json para funcionar no PostMan

---
+++ FirstWebApplication1NetCore/Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using FirstWebApplication1NetCore.DataStore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApplication1NetCore.Controllers
+{
+  //[Route("api/[controller]")]
+  [Route("api/cities")]
+  public class CitiesController : Controller
+  {
+
+    // [HttpGet("api/cities")]
+    public JsonResult GetCities()
+    {
+      return new JsonResult(CitiesDataStore.Current.Cities);
+    }
+    [HttpGet("{id}")]
+    public JsonResult GetCity(int id)
+    {
+      return new JsonResult(
+        CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id));
+    }
+  }
+}
