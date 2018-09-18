08dcb22a82fe7762b42b73367920b40d3bbf3d25
Guilherme Mascarenhas <gmascb@gmail.com>
2018-09-17 23:05:13 -0300

    Aprendendo: NamingStrategy, OutPutFormatterXML, DataAnnotationsModels

--- FirstWebApplication1NetCore/Models/CalculadoraModel.cs
+++ FirstWebApplication1NetCore/Models/CalculadoraModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,15 @@ namespace FirstWebApplication1NetCore.Models
 {
   public class CalculadoraModel
   {
+    [Required]
     public decimal valorinicial { get; set; }
+
+    [Required]
     public double meses { get; set; }
+
+    //[Required]
+    [MaxLength(200)]
+    public string descricao { get; set; }
+
   }
 }
--- FirstWebApplication1NetCore/Startup.cs
+++ FirstWebApplication1NetCore/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace FirstWebApplication1NetCore
 {
@@ -15,7 +17,24 @@ namespace FirstWebApplication1NetCore
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddMvc();
+      services.AddMvc().AddJsonOptions(options => {
+
+        if(options!=null)
+        {
+          //opção de não fazer cast nos nomes de objetos da classe.
+          //desta forma o Json Result será apresentado do jeito que foi nomeado na classe
+          var castedResolver = options.SerializerSettings.ContractResolver 
+          as DefaultContractResolver;
+
+          castedResolver.NamingStrategy = null;
+        }
+
+      });
+
+      //adiciona o retorno como XML ao invés do JSON
+      services.AddMvc().AddMvcOptions(options => options.OutputFormatters.Add(
+        new XmlDataContractSerializerOutputFormatter()));
+
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
